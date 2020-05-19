952a3190f31b01e4df0a76474459d76cae9b86e3
Recep Orkun Çevik <receporkuncevik@hotmail.com>
2020-05-19 17:55:44 +0300

    main düzeltildi

--- src/views/Main.java
+++ src/views/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application{
     }
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("home.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("doktor.fxml"));
         primaryStage.setTitle("Dentist Appointment");
         primaryStage.setScene(new Scene(root,1038,881));
         primaryStage.show();
